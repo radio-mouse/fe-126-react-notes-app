007439758f057ac10ffd8dde0e25163b7472d6e7
RadioMouse <aliaksandr@remote70065.local>
2023-04-05 13:17:46 +0300

    feat: interface

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -3,6 +3,6 @@ import { createRoot } from 'react-dom/client';
 
 import App from './components/App/App';
 
-const root = createRoot(document.getElementById('app'));
+// const root = createRoot(document.getElementById('app'));
 
-root.render(<App />);
+// root.render(<App />);
--- src/js/components/App/App.jsx
+++ src/js/components/App/App.jsx
@@ -1,10 +1,5 @@
 import React from 'react';
 
-const App = () => (
-  <main className="container-md py-4">
-    <h1 className="display-1">All Your Notes Are Here</h1>
-    <button type="button" className="btn btn-outline-success">Create Note</button>
-  </main>
-);
+const App = () => <div />;
 
 export default App;
